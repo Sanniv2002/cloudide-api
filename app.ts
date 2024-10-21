52c9091cbf1fd4b31dd16cf3a07551d23156e8c3
sanniv1020 <sanniv@useteleport.com>
2024-10-21 21:35:43 +0530

    cors config

--- app.ts
+++ app.ts
@@ -10,6 +10,21 @@ import apiRoutes from './routes/api';
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',').map(origin=>origin);
+app.use(CORS(
+    {
+        origin: (origin, callback) => {
+            allowedOrigins!.includes(origin as string) ? callback(null, true) : callback(new Error('Not allowed by CORS'))
+        },
+        allowedHeaders: [
+          'access-control-allow-origin',
+          'authorization',
+          'Pragma',
+          'contact',
+        ],
+        exposedHeaders: []
+      }
+));
 app.get('/', (_, res) => {
   res.status(200).json({ message: 'Server is healthy' });
 });
@@ -18,7 +33,6 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(limiter);
-app.use(CORS());
 app.use(express.json());
 
 app.use(authRoutes);
