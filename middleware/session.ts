f9d1bbec5113145807dce774961f1623218bafd6
sanniv1020 <sanniv@useteleport.com>
2024-10-23 00:45:18 +0530

    prod cookei issue

--- middleware/session.ts
+++ middleware/session.ts
@@ -3,8 +3,8 @@ import session from 'express-session';
 export const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET as string,
   cookie: {
-    secure: process.env.NODE_ENV === "auto",
-    sameSite: process.env.NODE_ENV === "none",
+    secure: "auto",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
    },
   resave: false,
