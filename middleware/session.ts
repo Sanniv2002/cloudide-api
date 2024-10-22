a80a3972e4b21cd7e380e22083db570d93152686
sanniv1020 <sanniv@useteleport.com>
2024-10-23 00:46:59 +0530

    prod cookei issue

--- middleware/session.ts
+++ middleware/session.ts
@@ -3,10 +3,11 @@ import session from 'express-session';
 export const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET as string,
   cookie: {
-    secure: "auto",
-    sameSite: "none",
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
-   },
+    domain: '.vercel.app',
+  },
   resave: false,
   saveUninitialized: false,
 });
