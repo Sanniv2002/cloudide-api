0c5075ebeff93eb991b3aeb49ddfb4bdbffcdf51
sanniv1020 <sanniv@useteleport.com>
2024-10-23 14:25:55 +0530

    prod cookei issue

--- middleware/session.ts
+++ middleware/session.ts
@@ -6,7 +6,6 @@ export const sessionMiddleware = session({
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    domain: '.vercel.app',
   },
   resave: false,
   saveUninitialized: false,
